51722b7ce35d7361a7261058f1d720375f7f8d96
Heikki Rauhala <rauhahe@heikki-rauhalas-computer-2.local>
2009-01-21 10:48:12 +0200

    Now its nice enough.

--- phpbb2_open_posts_since_last_visit_in_tabs.user.js
+++ phpbb2_open_posts_since_last_visit_in_tabs.user.js
@@ -20,26 +20,53 @@
 // First install Greasemonkey from http://greasemonkey.mozdev.org/
 // Then install this script by revisiting this page
 
+var allLinks = collectAllLinks();
+insertActionToPage(allLinks);
+GM_log("Added open all topics in tabs to page.");
+
+function collectAllLinks() {
+    return xpath("//a[contains(@href, 'viewtopic.php?p=')]");
+}
+
+function insertActionToPage(allLinks) {
+    var newTableCell;
+    newTableCell = createNewTableCell();
+    newTableCell.appendChild(createLinkAnchor(allLinks));
+    insertElementAfter(lookupForumIndexCell(), newTableCell);
+}
+
+function createNewTableCell() {
+    var newTableCell;
+    newTableCell = document.createElement('td');
+    newTableCell.class = "gensmall";
+    newTableCell.align = "right";
+    newTableCell.valign = "bottom";
+    return newTableCell;
+}
+
+function createLinkAnchor(allLinks) {
+    var linkAnchor;
+    linkAnchor = document.createElement('a');
+    linkAnchor.href = "#";
+    linkAnchor.innerHTML = 'Open all topics in tabs';
+    linkAnchor.addEventListener('click', function() {openRecentPosts(allLinks)}, true);
+    return linkAnchor;
+}
+
+function insertElementAfter(original, newElement) {
+    original.parentNode.insertBefore(newElement, original.nextSibling);
+}
+
+function lookupForumIndexCell() {
+    return xpath("//td/span/a[contains(@href, 'index.php')]/parent::*/parent::*").snapshotItem(0);
+}
+
 function openRecentPosts(allLinks) {
     for (var i = 0; i < allLinks.snapshotLength; i++) {
 	GM_openInTab(allLinks.snapshotItem(i).href);
     }
 }
-    
 function xpath(query) {
     return document.evaluate(query, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-}
-    allLinks = xpath("//a[contains(@href, 'viewtopic.php?p=')]");
+};
     
-    forumIndexLinkCell = xpath("//td/span/a[contains(@href, 'index.php')]/parent::*/parent::*").snapshotItem(0);
-    
-    newTableCell = document.createElement('td');
-    newTableCell.class = "gensmall";
-    newTableCell.align = "right";
-    newTableCell.valign = "bottom";
-    linkAnchor = document.createElement('a');
-    linkAnchor.innerHTML = '<a href="#" class="gensmall">Open all topics in tabs</a>';
-    linkAnchor.addEventListener('click', function() {openRecentPosts(allLinks)}, true);
-    newTableCell.appendChild(linkAnchor);
-
-    forumIndexLinkCell.parentNode.insertBefore(newTableCell, forumIndexLinkCell.nextSibling);
